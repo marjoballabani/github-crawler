041ae2a80d7027850e31d7102d14fe765cabcb54
mballabani <mballabani@helius-software.com>
2018-07-06 22:48:21 +0200

    fix: release

--- src/actions/repoActions.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const baseURI = "https://api.github.com/users/facebook/repos"
-
-export const fetchRepos = () => ({
-  type: "GET_REPO",
-  payload: axios.get(baseURI)
-})
-
